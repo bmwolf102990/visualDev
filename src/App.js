29f1df80e257089dfa75352c91ee5e8e3acff7f1
Bryan M. Wolf <bmwolf.professional@gmail.com>
2023-02-09 06:21:43 -0700

    added 'set' and 'run' buttons

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ function App() {
     const [collectionSize, setCollectionSize] = useState(0)
     const [targetNum, setTargetNum] = useState(0)
     const [runStatus, setRunStatus] = useState(false)
-    
+
     return (
     <>
         <label>
-            Set Collection Size: <input type="number" min="0" max="500"/>
+            Set Collection Size: <input type="number" min="0" max="500" />
         </label>
-
         <label>
-            Set Target Number: <input type="number" min="1" max="500"/>
+            Set Target Number: <input type="number" min="1" max="500" />
         </label>
+        <button type="submit" value="Set Collection & Target" />
+        <button type="submit" value="Run Binary Search" />
     </>
     );
 }

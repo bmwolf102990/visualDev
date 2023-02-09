b479c5b86270af9a9a5adcff4228a7b5fdc2dbb3
Bryan M. Wolf <bmwolf.professional@gmail.com>
2023-02-09 08:27:06 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
     const [collection, setCollection] = useState([])
-    const [targetNum, setTargetNum] = useState(0)
+    const [targetNum, setTargetNum] = useState(1)
     const [runStatus, setRunStatus] = useState(false)
 
     useEffect(() => {
@@ -16,20 +16,25 @@ function App() {
         }
         handleCollectionGeneration()
     }, [])
-
+    console.log(targetNum)
     return (
     <>
         <label>
-            Target Number: <input type="number" min="1" max={collection.length} />
+            Target Number: <input type="number" min="1" max={collection.length} placeholder="1-1080" onChange={(e) => setTargetNum(e.target.value)} />
         </label>
+        {
+        targetNum < 1 || targetNum > 1080 || targetNum === "" ?
+        alert("INVALID TARGET: Please enter a number between 1 and 1080")
+        :null
+        }
         <button type="submit" value="set">Set Target Number</button>
         <button type="submit" value="run">Run Binary Search</button>
         <div className="flex-container">
-            {
-                collection ?
-                collection.map((unit) => <div key={unit} className={`collection-unit unit-${unit}`}></div>)
-                :null
-            }
+        {
+        collection ?
+        collection.map((unit) => <div key={unit} className={`collection-unit unit-${unit}`}></div>)
+        :null
+        }
         </div>
     </>
     );

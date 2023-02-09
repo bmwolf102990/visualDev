bd512d67c753bab6fa4571f5baf47f4a9ddbed24
Bryan M. Wolf <bmwolf.professional@gmail.com>
2023-02-09 13:35:29 -0700

    updated App.js file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     useEffect(() => {
         const handleCollectionGeneration = () => {
             let generatedCollection = []
-            for(let i=1; i < 1081; i++) {
+            for(let i=1; i <= 1080; i++) {
                 generatedCollection.push(i);
             }
             setCollection(generatedCollection);
@@ -17,7 +17,43 @@ function App() {
         handleCollectionGeneration()
     }, [])
 
-    const handleSearch = () => {}
+    const handleSearch = () => {
+        let start = 1;
+        let end = collection.length;
+        let timeout = 0;
+        
+        while(start <= end) {
+            let mid = Math.floor((start + end) / 2);
+
+            if(collection[mid] === targetNum - 1) {
+                setTimeout(() => {
+                    $(`.collection-unit`).css("background-color","#6c757d");
+                    $(`#unit-${targetNum}`).css("background-color","#ffc107");
+                }, timeout += 750);
+                return mid;
+            }
+            else if(collection[mid] < targetNum - 1) {
+                start = mid + 1;
+                setTimeout(() => {
+                    $(`.collection-unit`).css("background-color","#6c757d");
+                    $(`#unit-${targetNum}`).css("background-color","#20c997");
+                    $(`#unit-${mid}`).css("background-color","#0dcaf0");
+                }, timeout += 750);
+                continue;
+            }
+            else if(collection[mid] > targetNum - 1) {
+                end = mid - 1;
+                setTimeout(() => {
+                    $(`.collection-unit`).css("background-color","#6c757d");
+                    $(`#unit-${targetNum}`).css("background-color","#20c997");
+                    $(`#unit-${mid}`).css("background-color","#0dcaf0");
+                }, timeout += 750);
+                continue;
+            }
+            return -1;
+        }
+
+    }
     
     return (
     <>
@@ -29,8 +65,8 @@ function App() {
         <label>
             Target Number: <input type="number" min="1" max={collection.length} placeholder="1-1080" onChange={(e) => setTargetNum(e.target.value)} />
         </label>
-        <button type="submit" value="set" onClick={() => $(`.collection-unit`).css("background-color","#6c757d") && $(`#unit-${targetNum}`).css("background-color","#ffc107")}>Mark Target Number</button>
-        <button type="submit" value="run" onClick={handleSearch}>Run Binary Search</button>
+        <button type="submit" value="set" onClick={() => $(`.collection-unit`).css("background-color","#6c757d") && $(`#unit-${targetNum}`).css("background-color","#20c997")}>Mark Target Number</button>
+        <button type="submit" value="run" onClick={() => handleSearch()}>Run Binary Search</button>
         <div className="flex-container">
             {
             collection ?

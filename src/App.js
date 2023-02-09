1e09fb1f0656e8784979e625ce197f6be8d9ceba
Bryan M. Wolf <bmwolf.professional@gmail.com>
2023-02-09 07:41:22 -0700

    updated App.css, App.js and added in bootstrap CDN

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
     const [collection, setCollection] = useState([])
-    const [collectionSize, setCollectionSize] = useState(0)
     const [targetNum, setTargetNum] = useState(0)
     const [runStatus, setRunStatus] = useState(false)
 
+    useEffect(() => {
+        const handleCollectionGeneration = () => {
+            let generatedCollection = []
+            for(let i=1; i < 1081; i++) {
+                generatedCollection.push(i);
+            }
+            setCollection(generatedCollection);
+        }
+        handleCollectionGeneration()
+    }, [])
     return (
     <>
-        <label>
-            Set Collection Size: <input type="number" min="0" max="500" />
-        </label>
-        <label>
-            Set Target Number: <input type="number" min="1" max="500" />
-        </label>
-        <button type="submit" value="Set Collection & Target" />
-        <button type="submit" value="Run Binary Search" />
+        <div className="flex-container">
+            {
+                collection ?
+                collection.map((unit) => <div key={unit} className="collection-unit"></div>)
+                :null
+            }
+        </div>
     </>
     );
 }

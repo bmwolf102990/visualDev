c04c22229903b3bb0f1a9181cee770fe8db73c2a
Bryan M. Wolf <bmwolf.professional@gmail.com>
2023-02-09 06:18:25 -0700

    updated App.js and App.css files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState } from 'react'
 import './App.css';
 
-function App() {return (<></>);}
+function App() {
+    const [collection, setCollection] = useState([])
+    const [collectionSize, setCollectionSize] = useState(0)
+    const [targetNum, setTargetNum] = useState(0)
+    const [runStatus, setRunStatus] = useState(false)
+    
+    return (
+    <>
+        <label>
+            Set Collection Size: <input type="number" min="0" max="500"/>
+        </label>
+
+        <label>
+            Set Target Number: <input type="number" min="1" max="500"/>
+        </label>
+    </>
+    );
+}
 
 export default App;

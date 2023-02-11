34b04ba9d26b682966f51c39f2fdb9daf876a5f9
Bryan M. Wolf <bmwolf.professional@gmail.com>
2023-02-11 09:57:30 -0700

    added bootstrap for styling. added target click-select functionality in App.js file. minimized css styling in App.css

--- src/App.js
+++ src/App.js
@@ -14,80 +14,89 @@ const App = () => {
             }
             setCollection(generatedCollection);
         }
-        handleCollectionGeneration()
-    }, [])
+        handleCollectionGeneration();
+
+        const handleTargetMark = () => {
+            $(`.collection-unit`).css("background-color","#6c757d");
+            $(`#unit-${targetNum}`).css("background-color","#fd7e14");
+        }
+        handleTargetMark();
+    }, [targetNum])
 
     const handleSearch = () => {
-        var start = 1;
-        var end = collection.length;
-        var timeout = 0;
+        if(targetNum === 0) {
+            alert("you must first choose a target by clicking on a unit in the collection");
+        }
+        else {
+            $(`#run`).attr("disabled",true);
+            $(`#clear`).attr("disabled",true);
+            var start = 1;
+            var end = collection.length;
+            var timeout = 0;
 
-        $(`#unit-${start}`).css("background-color","#dc3545");
-        $(`#unit-${start}`).nextUntil(`#unit-${end}`).css("background-color","#0dcaf0");
-        $(`#unit-${targetNum}`).css("background-color","#fd7e14");
-        $(`#unit-${end}`).css("background-color","#dc3545");
-        
-        while(start <= end) {
-            let startLimit = start;
-            let endLimit = end;
-            let mid = Math.floor((start + end) / 2);
+            $(`#unit-${start}`).css("background-color","#dc3545");
+            $(`#unit-${start}`).nextUntil(`#unit-${end}`).css("background-color","#0dcaf0");
+            $(`#unit-${targetNum}`).css("background-color","#fd7e14");
+            $(`#unit-${end}`).css("background-color","#dc3545");
+            
+            while(start <= end) {
+                let startLimit = start;
+                let endLimit = end;
+                let mid = Math.floor((start + end) / 2);
 
-            // $(`#unit-${mid}`).css("background-color","#6f42c1");
+                // $(`#unit-${mid}`).css("background-color","#6f42c1");
 
-            if(mid === targetNum - 1 || mid + 1 === targetNum - 1 || mid - 1 === targetNum - 1) {
-                setTimeout(() => {
-                    $(`.collection-unit`).css("background-color","#6c757d");
-                    $(`#unit-${targetNum}`).css("background-color","#ffc107");
-                }, timeout += 1000);
-                return mid;
+                if(mid === targetNum - 1 || mid + 1 === targetNum - 1 || mid - 1 === targetNum - 1) {
+                    setTimeout(() => {
+                        $(`.collection-unit`).css("background-color","#6c757d");
+                        $(`#unit-${targetNum}`).css("background-color","#ffc107");
+                        $(`#run`).attr("disabled",false);
+                        $(`#clear`).attr("disabled",false);
+                    }, timeout += 1000);
+                    return mid;
+                }
+                else if(mid < targetNum - 1) {
+                    start = mid + 1;
+                    startLimit = start;
+                    endLimit = end;
+                    setTimeout(() => {
+                        $(`.collection-unit`).css("background-color","#6c757d");
+                        $(`#unit-${startLimit}`).css("background-color","#dc3545");
+                        $(`#unit-${startLimit}`).nextUntil(`#unit-${endLimit}`).css("background-color","#0dcaf0");
+                        $(`#unit-${targetNum}`).css("background-color","#fd7e14");
+                        $(`#unit-${endLimit}`).css("background-color","#dc3545");
+                    }, timeout += 1000);
+                    continue;
+                }
+                else if(mid > targetNum - 1) {
+                    end = mid - 1;
+                    startLimit = start;
+                    endLimit = end;
+                    setTimeout(() => {
+                        $(`.collection-unit`).css("background-color","#6c757d");
+                        $(`#unit-${startLimit}`).css("background-color","#dc3545");
+                        $(`#unit-${startLimit}`).nextUntil(`#unit-${endLimit}`).css("background-color","#0dcaf0");
+                        $(`#unit-${targetNum}`).css("background-color","#fd7e14");
+                        $(`#unit-${endLimit}`).css("background-color","#dc3545");
+                    }, timeout += 1000);
+                    continue;
+                }
+                return -1;
             }
-            else if(mid < targetNum - 1) {
-                start = mid + 1;
-                startLimit = start;
-                endLimit = end;
-                setTimeout(() => {
-                    $(`.collection-unit`).css("background-color","#6c757d");
-                    $(`#unit-${startLimit}`).css("background-color","#dc3545");
-                    $(`#unit-${startLimit}`).nextUntil(`#unit-${endLimit}`).css("background-color","#0dcaf0");
-                    $(`#unit-${targetNum}`).css("background-color","#fd7e14");
-                    $(`#unit-${endLimit}`).css("background-color","#dc3545");
-                }, timeout += 1000);
-                continue;
-            }
-            else if(mid > targetNum - 1) {
-                end = mid - 1;
-                startLimit = start;
-                endLimit = end;
-                setTimeout(() => {
-                    $(`.collection-unit`).css("background-color","#6c757d");
-                    $(`#unit-${startLimit}`).css("background-color","#dc3545");
-                    $(`#unit-${startLimit}`).nextUntil(`#unit-${endLimit}`).css("background-color","#0dcaf0");
-                    $(`#unit-${targetNum}`).css("background-color","#fd7e14");
-                    $(`#unit-${endLimit}`).css("background-color","#dc3545");
-                }, timeout += 1000);
-                continue;
-            }
-            return -1;
         }
 
     }
-    
+
     return (
     <>
-        {
-        targetNum < 0 || targetNum > 1000 ?
-        alert("INVALID TARGET: Please enter a number between 1 and 1000...")
-        :null
-        }
-        <label>
-            Target Number: <input id="target-input" type="number" min="1" max={collection.length} placeholder="1-1000" onChange={(e) => setTargetNum(e.target.value)} />
-        </label>
-        <button id="set-btn" type="submit" value="set" onClick={() => $(`.collection-unit`).css("background-color","#6c757d") && $(`#unit-${targetNum}`).css("background-color","#fd7e14")}>Mark Target Number</button>
-        <button id="run-btn" type="submit" value="run" onClick={() => handleSearch()}>Run Binary Search</button>
-        <div className="flex-container">
+        <div className="d-flex flex-wrap align-items-center justify-content-start mx-2 my-2">
+            <button id="run" className="btn btn-outline-light  mx-1" type="submit" onClick={handleSearch}>Run Binary Search</button>
+            <button id="clear" className="btn btn-outline-info mx-1 px-5" type="submit" onClick={() => setTargetNum(0)}>Clear</button>
+        </div>
+        <div className="d-flex flex-wrap align-items-center justify-content-center">
             {
             collection ?
-            collection.map((unit) => <div key={unit} id={`unit-${unit}`} className="collection-unit"></div>)
+            collection.map((unit) => <div key={unit} title={unit} id={`unit-${unit}`} className="collection-unit" onClick={(e) => setTargetNum(Number(e.target.title))}></div>)
             :null
             }
         </div>

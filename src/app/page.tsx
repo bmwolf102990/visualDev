c8944163c51b0ece8449553cbaa88f574d741de6
bmwolf102990 <bmwolf.professional@gmail.com>
2023-09-27 14:04:35 -0700

    Added Footer component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { Nav } from "../components/Nav/Nav";
+import { Footer } from "../components/Footer/Footer";
 
 export default function Home() {
   return (
     <main>
       <Nav />
+      <div className="h-[625px]">
+
+      </div>
+      <Footer />
     </main>
   )
 }

ced40c22025729ecd3339665564746fdeca60363
bmwolf102990 <bmwolf.professional@gmail.com>
2023-09-27 08:36:24 -0700

    Updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import Image from 'next/image'
+import { Nav } from "../components/Nav/Nav";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        src="/visualdev.svg"
-        alt="VisualDev Logo"
-        className=""
-        width={100}
-        height={24}
-        priority
-      />
+    <main>
+      <Nav />
     </main>
   )
 }

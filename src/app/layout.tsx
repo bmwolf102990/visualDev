1046751e1419aff5a1801a8811aee4b0c4f95e87
bmwolf102990 <bmwolf.professional@gmail.com>
2023-09-27 08:35:55 -0700

    Updated font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  weight: ['100', '500', '900'],
+  subsets: ['latin']
+});
 
 export const metadata: Metadata = {
   title: 'visualDev',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }

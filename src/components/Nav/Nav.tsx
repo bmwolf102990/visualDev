0c96f2e74182710dfabbe9b1d09bb2a950ccb0da
bmwolf102990 <bmwolf.professional@gmail.com>
2023-09-27 14:03:09 -0700

    Updated Nav component

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,29 +1,13 @@
 "use client"
 import React from "react";
 import Image from "next/image";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import { BsHeartFill } from "react-icons/bs";
 
 export const Nav = () => {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-    const menuItems = [
-        "Profile",
-        "Dashboard",
-        "Activity",
-        "Analytics",
-        "System",
-        "Deployments",
-        "My Settings",
-        "Team Settings",
-        "Help & Feedback",
-        "Log Out",
-    ];
 
     return(
-        <Navbar className="justify-start" onMenuOpenChange={setIsMenuOpen}>
-            <NavbarMenuToggle
-                aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-            />
+        <Navbar maxWidth="full">
             <NavbarBrand>
                 <Image 
                     src="/visualdev.svg"
@@ -37,27 +21,12 @@ export const Nav = () => {
             </NavbarBrand>
             <NavbarContent justify="end">
                 <NavbarItem>
-                    <Button as={Link} color="primary" href="#" variant="flat">
+                    <Button as={Link} color="secondary" href="#" variant="flat">
+                        <BsHeartFill fill="#ec4899" />
                         Sponsor
                     </Button>
                 </NavbarItem>
             </NavbarContent>
-            <NavbarMenu>
-            {menuItems.map((item, index) => (
-                <NavbarMenuItem key={`${item}-${index}`}>
-                    <Link
-                    color={
-                        index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-                    }
-                    className="w-full"
-                    href="#"
-                    size="lg"
-                    >
-                    {item}
-                    </Link>
-                </NavbarMenuItem>
-            ))}
-            </NavbarMenu>
         </Navbar>
     );
 };

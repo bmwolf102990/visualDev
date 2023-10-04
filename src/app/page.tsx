da4819ab1a3785f14413d20edd1fdec0c4815d3f
bmwolf102990 <bmwolf.professional@gmail.com>
2023-10-04 09:31:27 -0700

    Updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { TopNav } from "../components/TopNav/TopNav";
 import { SideNav } from "../components/SideNav/SideNav";
 import { Footer } from "../components/Footer/Footer";
 import { VDSphere } from "../components/VDSphere/VDSphere";
+import { ImEye } from "react-icons/im";
+import Image from "next/image";
 import Typewriter from 'typewriter-effect'
 
 export default function Home() {
@@ -16,28 +18,40 @@ export default function Home() {
         display={display}
         setDisplay={setDisplay}
       />
-      <div className="h-[600px] grid grid-cols-4">
+      <div className="h-[600px] grid grid-cols-5">
         {display === "home" && (
           <>
             <SideNav
               setDisplay={setDisplay}
             />
-            {/* <div className="col-start-2 col-end-4 text-center">
-              <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quaerat eum eos totam quam ab perspiciatis incidunt necessitatibus recusandae porro, architecto nesciunt harum laudantium mollitia, adipisci quos ducimus debitis vero. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              </span>
-              <Typewriter
-                options={{
-                  strings: [
-                    "string 1",
-                    "string 2"
-                  ],
-                  wrapperClassName:'text-pink',
-                  cursorClassName: '',
-                  autoStart: true,
-                  loop: true,
-                }}
+            {/* <div className="col-start-4 col-end-6 self-center p-24 text-justify">
+              <Image
+                className="mx-auto"
+                src="/visualdev.svg"
+                alt=""
+                width={100}
+                height={50}
               />
+              <span className="font-thin">
+              visualDev is a Web Development/SWE learning platform aimed at providing visual aid tools, along with supplemental resources, to allow you to learn by </span><ImEye className="scale-150 inline mx-1" fill="#ec4899" /><span className="font-thin"> the workings of:</span>
+              <div className="my-5">
+                <Typewriter
+                  options={{
+                    strings: [
+                      "Lorem Ipsum 0",
+                      "Lorem Ipsum 1",
+                      "Lorem Ipsum 2",
+                      "Lorem Ipsum 3",
+                      "Lorem Ipsum 4",
+                      "Lorem Ipsum 5",
+                    ],
+                    wrapperClassName:'text-pink',
+                    cursorClassName: '',
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </div>
             </div> */}
             <VDSphere />
           </>

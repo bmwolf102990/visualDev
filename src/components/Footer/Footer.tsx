94bcd6e4239a38a4c4b7a44d027e3b2ab709fdf4
bmwolf102990 <bmwolf.professional@gmail.com>
2023-10-03 19:41:35 -0700

    Updated Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,8 +5,8 @@ import { BsHeart } from "react-icons/bs";
 export const Footer = () => {
 
     return(
-        <footer className="p-5 text-sm text-center">
-            <div className="p-1">
+        <footer className="text-sm text-center">
+            <div>
                 <span><AiOutlineCopyright className="inline"/> 2023 visualDev</span>
             </div>
             <span>developed with <BsHeart className="inline"/> by <a className="underline hover:text-pink" href="https://github.com/bmwolf102990">bryan wolf</a> & <a className="underline hover:text-pink" href="#">open source community</a></span>

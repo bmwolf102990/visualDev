388177643fd04701fcc7ec6372915ebe4bd252af
bmwolf102990 <bmwolf.professional@gmail.com>
2023-10-04 14:51:09 -0700

    Updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import { TopNav } from "../components/TopNav/TopNav";
 import { SideNav } from "../components/SideNav/SideNav";
 import { Footer } from "../components/Footer/Footer";
 import { VDSphere } from "../components/VDSphere/VDSphere";
-// import { ImEye } from "react-icons/im";
-// import Image from "next/image";
-// import Typewriter from 'typewriter-effect'
+import { ImEye } from "react-icons/im";
+import Image from "next/image";
+import Typewriter from 'typewriter-effect'
 
 export default function Home() {
   const [display, setDisplay] = useState("home");
@@ -18,13 +18,13 @@ export default function Home() {
         display={display}
         setDisplay={setDisplay}
       />
-      <div className="h-[600px] grid grid-cols-5">
+      <div className="h-[600px] grid grid-cols-4">
         {display === "home" && (
           <>
             <SideNav
               setDisplay={setDisplay}
             />
-            {/* <div className="col-start-4 col-end-6 self-center p-24 text-justify">
+            <div className="z-10 col-start-2 col-end-4 self-center p-24 text-justify">
               <Image
                 className="mx-auto"
                 src="/visualdev.svg"
@@ -32,9 +32,10 @@ export default function Home() {
                 width={100}
                 height={50}
               />
+              <hr className="text-pink pb-3" />
               <span>
-              visualDev is a Web Development/SWE learning platform aimed at providing visual aid tools, along with supplemental resources, to allow you to learn by actually </span><ImEye className="scale-150 inline mx-1" fill="#ec4899" /><span> the workings of:</span>
-              <div className="my-5">
+              visualDev is a Web Development/SWE learning platform aimed at providing visual learning tools, along with supplemental resources, to allow you to learn by actually </span><ImEye className="scale-150 inline mx-1" fill="#ec4899" /><span> the workings of:</span>
+              <div className="h-[100px] my-5 text-2xl text-center">
                 <Typewriter
                   options={{
                     strings: [
@@ -45,14 +46,14 @@ export default function Home() {
                       "Lorem Ipsum 4",
                       "Lorem Ipsum 5",
                     ],
-                    wrapperClassName:'text-pink',
-                    cursorClassName: '',
+                    wrapperClassName:'',
+                    cursorClassName: 'text-pink',
                     autoStart: true,
                     loop: true,
                   }}
                 />
               </div>
-            </div> */}
+            </div>
             {/* ===== BACKGROUND ANIMATION ===== */}
             <VDSphere />
             {/* ===== BACKGROUND ANIMATION ===== */}

e24da52161671b8e3a66756119c6407360d59625
bmwolf102990 <bmwolf.professional@gmail.com>
2023-10-05 13:08:40 -0700

    Created ToolCanvas dir and component file

---
+++ src/components/ToolCanvas/ToolCanvas.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BinarySearchVisualizer } from "../BinarySearchVisualizer/BinarySearchVisualizer";
+
+export const ToolCanvas = () => {
+
+
+    return(
+        <div className="col-span-4 mx-7 mb-2">
+            <BinarySearchVisualizer />
+        </div>
+    );
+};

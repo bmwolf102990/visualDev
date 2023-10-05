9f3a79e0c5b1d2afc1d015f59a8cb7e795ab9644
bmwolf102990 <bmwolf.professional@gmail.com>
2023-10-05 13:06:12 -0700

    Updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,10 @@
 import { useState } from "react";
 import { TopNav } from "../components/TopNav/TopNav";
 import { SideNav } from "../components/SideNav/SideNav";
-import { Footer } from "../components/Footer/Footer";
+import { MissionStatement } from "../components/MissionStatement/MissionStatement";
 import { VDSphere } from "../components/VDSphere/VDSphere";
-import { ImEye } from "react-icons/im";
-import Image from "next/image";
-import Typewriter from 'typewriter-effect'
+import { ToolCanvas } from "../components/ToolCanvas/ToolCanvas";
+import { Footer } from "../components/Footer/Footer";
 
 export default function Home() {
   const [display, setDisplay] = useState("home");
@@ -24,43 +23,14 @@ export default function Home() {
             <SideNav
               setDisplay={setDisplay}
             />
-            <div className="z-10 col-start-2 col-end-4 self-center p-24 text-justify">
-              <Image
-                className="mx-auto"
-                src="/visualdev.svg"
-                alt=""
-                width={100}
-                height={50}
-              />
-              <hr className="text-pink pb-3" />
-              <span>
-              visualDev is a Web Development/SWE learning platform aimed at providing visual learning tools, along with supplemental resources, to allow you to learn by actually </span><ImEye className="scale-150 inline mx-1" fill="#ec4899" /><span> the workings of:</span>
-              <div className="h-[100px] my-5 text-2xl text-center">
-                <Typewriter
-                  options={{
-                    strings: [
-                      "Lorem Ipsum 0",
-                      "Lorem Ipsum 1",
-                      "Lorem Ipsum 2",
-                      "Lorem Ipsum 3",
-                      "Lorem Ipsum 4",
-                      "Lorem Ipsum 5",
-                    ],
-                    wrapperClassName:'',
-                    cursorClassName: 'text-pink',
-                    autoStart: true,
-                    loop: true,
-                  }}
-                />
-              </div>
-            </div>
+            <MissionStatement />
             {/* ===== BACKGROUND ANIMATION ===== */}
             <VDSphere />
             {/* ===== BACKGROUND ANIMATION ===== */}
           </>
         )}
         {display !== "home" && (
-          <div className="col-span-4 mx-7 my-3"></div>
+          <ToolCanvas />
         )}
       </div>
       <Footer />

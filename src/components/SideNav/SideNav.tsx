ac4a17e11989dc65916fe4cd3ae8f0f2ae71bb62
bmwolf102990 <bmwolf.professional@gmail.com>
2023-10-06 11:56:22 -0700

    Removed placeholder items from list in SideNav

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -13,14 +13,6 @@ export const SideNav = ({ setDisplay }) => {
     return (
         <ScrollShadow hideScrollBar isEnabled={false} className="h-[600px] grid grid-cols-1">
             <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Binary Search</Link>
-            {/* <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>
-            <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>
-            <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>
-            <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>
-            <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>
-            <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>
-            <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>
-            <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link> */}
         </ScrollShadow>
   );
 }

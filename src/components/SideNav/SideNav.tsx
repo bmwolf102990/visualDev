a5afba1c9df2cee3b73460ca6d691aeaadc3b394
bmwolf102990 <bmwolf.professional@gmail.com>
2023-10-06 11:17:38 -0700

    Updated component

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -12,7 +12,7 @@ export const SideNav = ({ setDisplay }) => {
 
     return (
         <ScrollShadow hideScrollBar isEnabled={false} className="h-[600px] grid grid-cols-1">
-            <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>
+            <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Binary Search</Link>
             <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>
             <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>
             <Link className="p-10 border-l-4 border-purple hover:bg-gradient-to-r from-purple to-black" href="#" onClick={handleSetDisplay}>Lorem Ipsum</Link>

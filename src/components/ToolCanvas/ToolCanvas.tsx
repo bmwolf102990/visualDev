dc76d82dce88163c35dcfc1b4481d530b299947d
bmwolf102990 <bmwolf.professional@gmail.com>
2023-10-06 11:16:36 -0700

    Updated component

--- src/components/ToolCanvas/ToolCanvas.tsx
+++ src/components/ToolCanvas/ToolCanvas.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BinarySearchVisualizer } from "../BinarySearchVisualizer/BinarySearchVisualizer";
 
-export const ToolCanvas = () => {
+export const ToolCanvas = ({ display, setDisplay }) => {
 
 
     return(
         <div className="col-span-4 mx-7 mb-2">
-            <BinarySearchVisualizer />
+            {display === "binary search" && (
+                <BinarySearchVisualizer />
+            )}
         </div>
     );
 };
